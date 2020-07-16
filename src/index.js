de896e3269ce6f5e01e28edaa109482fa780826d
Muhammed Osama <60500495+MuhammedOsama@users.noreply.github.com>
2020-07-16 02:29:18 +0200

    init

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+// global style
+import 'bootstrap-v4-rtl/dist/css/bootstrap-rtl.min.css';
+import '@fortawesome/fontawesome-free/css/all.css';
+import './Layout/components/style/Main.scss';
 
 ReactDOM.render(
   <React.StrictMode>
