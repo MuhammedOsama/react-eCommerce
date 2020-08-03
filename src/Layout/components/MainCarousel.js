266c77b22c8a517fab75cb65c48ad52cda1b58ce
Mohamed Ghazi <m.mahmoud@uplift.plus>
2020-08-03 12:04:25 +0200

    Add carousel animation

--- src/Layout/components/MainCarousel.js
+++ src/Layout/components/MainCarousel.js
@@ -9,25 +9,25 @@ class MainCarousel extends Component {
                     <Carousel.Item>
                         <img className="d-block w-100" src="./master-slide-02.jpg" alt="First slide" />
                         <Carousel.Caption>
-                            <p>Woman Collection 2020</p>
-                            <h1>New Arrivals</h1>
-                            <button>Shop Now</button>
+                            <p id="collection-1">Woman Collection 2020</p>
+                            <h1 id="new-arrival-1">New Arrivals</h1>
+                            <button id="caption-btn-1">Shop Now</button>
                         </Carousel.Caption>
                     </Carousel.Item>
                     <Carousel.Item>
                         <img className="d-block w-100" src="./master-slide-03.jpg" alt="Second slide" />
                         <Carousel.Caption>
-                            <p>Woman Collection 2020</p>
-                            <h1>New Arrivals</h1>
-                            <button>Shop Now</button>
+                            <p id="collection-2">Woman Collection 2020</p>
+                            <h1 id="new-arrival-2">New Arrivals</h1>
+                            <button id="caption-btn-2">Shop Now</button>
                         </Carousel.Caption>
                     </Carousel.Item>
                     <Carousel.Item>
                         <img className="d-block w-100" src="./master-slide-04.jpg" alt="Third slide" />
                         <Carousel.Caption>
-                            <p>Woman Collection 2020</p>
-                            <h1>New Arrivals</h1>
-                            <button>Shop Now</button>
+                            <p id="collection-3">Woman Collection 2020</p>
+                            <h1 id="new-arrival-3">New Arrivals</h1>
+                            <button id="caption-btn-3">Shop Now</button>
                         </Carousel.Caption>
                     </Carousel.Item>
                 </Carousel>
