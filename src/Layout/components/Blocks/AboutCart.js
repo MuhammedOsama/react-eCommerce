1105f31c775b77b4a953b8184f7a4d8f20e94b5e
Muhammed Osama <60500495+MuhammedOsama@users.noreply.github.com>
2020-08-24 15:02:28 +0200

    Create Pages

---
+++ src/Layout/components/Blocks/AboutCart.js
@@ -0,0 +1,41 @@
+import React, { Component } from 'react';
+
+class AboutPage extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            items: [
+                {image: "./banner-14.jpg", alter: "About", header: "Our Story", desc: "Phasellus egestas nisi nisi, lobortis ultricies risus semper nec. Vestibulum pharetra ac ante ut pellentesque. Curabitur fringilla dolor quis lorem accumsan, vitae molestie urna dapibus. Pellentesque porta est ac neque bibendum viverra. Vivamus lobortis magna ut interdum laoreet. Donec gravida lorem elit, quis condimentum ex semper sit amet. Fusce eget ligula magna. Aliquam aliquam imperdiet sodales. Ut fringilla turpis in vehicula vehicula. Pellentesque congue ac orci ut gravida. Aliquam erat volutpat. Donec iaculis lectus a arcu facilisis, eu sodales lectus sagittis. Etiam pellentesque, magna vel dictum rutrum, neque justo eleifend elit, vel tincidunt erat arcu ut sem. Sed rutrum, turpis ut commodo efficitur, quam velit convallis ipsum, et maximus enim ligula ac ligula. Vivamus tristique vulputate ultricies. Sed vitae ultrices orci.", details: "Creativity is just connecting things. When you ask creative people how they did something, they feel a little guilty because they didn't really do it, they just saw something. It seemed obvious to them after a while", span: "- Steve Job's"}
+            ]
+        }
+    }
+
+    render() {
+        return(
+            <div id="about">
+                <div className="container">
+                        {this.state.items.map((item, id) => {
+                            return(
+                                <div key={id} className="row">
+                                    <div className="col-md-4 col-12">
+                                        <div className="pos-relative hov-img-zoom">
+                                            <img src={item.image} alt={item.alter}/>
+                                        </div>
+                                    </div>
+                                    <div className="col-md-8 col-12">
+                                        <h1>{item.header}</h1>
+                                        <p>{item.desc}</p>
+                                        <p>{item.details}</p>
+                                        <span>{item.span}</span>
+                                    </div>
+                                </div>
+                            );
+                        })}
+                </div>
+            </div>
+        );
+    }
+}
+
+export default AboutPage;
---
+++ src/Layout/components/Blocks/ContactPage.js
@@ -0,0 +1,79 @@
+import React, { Component } from 'react';
+import { Map, GoogleApiWrapper, Marker  } from 'google-maps-react';
+
+    const mapStyles = {
+        width: '100%',
+        height: '100%'
+    };
+
+class Contact extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            stores: [
+                {latitude: 47.49855629475769, longitude: -122.14184416996333},
+                {latitude: 47.359423, longitude: -122.021071},
+                {latitude: 47.2052192687988, longitude: -121.988426208496},
+                {latitude: 47.6307081, longitude: -122.1434325},
+                {latitude: 47.3084488, longitude: -122.2140121},
+                {latitude: 47.5524695, longitude: -122.0425407}
+            ]
+        }
+    }
+    
+    displayMarkers = () => {
+        return this.state.stores.map((store, index) => {
+            return <Marker key={index} id={index} position={{
+            lat: store.latitude,
+            lng: store.longitude
+            }}
+            onClick={() => console.log("You clicked me!")} />
+        });
+    }
+
+    render() {
+        return(
+            <div id="contact">
+                <div className="container">
+                    <div className="row">
+                        <div className="col-md-6">
+                            {/* <Map
+                                google={this.props.google}
+                                zoom={8}
+                                initialCenter={{ lat: 47.444, lng: -122.176}}
+                            >
+                                {this.displayMarkers()}
+                            </Map> */}
+
+                            <Map
+                                google={this.props.google}
+                                zoom={11}
+                                style={mapStyles}
+                                initialCenter={{
+                                    latitude: -1.2884,
+                                    longitude: 36.8233
+                                }}
+                            />
+
+                        </div>
+                        <div className="col-md-6">
+                            <h1>Send us your message</h1>
+                            <input type="text" placeholder="Full Name" />
+                            <input type="text" placeholder="Phone Number" />
+                            <input type="text" placeholder="E-mail Address" />
+                            <textarea placeholder="Message"></textarea>
+                            <button>Send</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+
+// export default Contact;
+
+export default GoogleApiWrapper({
+    apiKey: 'YOUR_GOOGLE_API_KEY_GOES_HERE'
+})(Contact);
